4de8d82b77252471a12ba7ecb62e903036fb9850
Sebastian Sandqvist <seb@sparque.me>
2017-05-09 11:23:32 -0700

    improve sound

--- app/actions/audio/index.js
+++ app/actions/audio/index.js
@@ -15,19 +15,24 @@ const freqMap = {
 	d: 783.99
 };
 
+const RAMP_DOWN_TIME = 0.01;
+
 export default actions({
 	playSound({ state }, color) {
 		const osc = state.ctx.createOscillator();
 		osc.connect(state.gainNode);
 		osc.type = 'sine';
 		osc.frequency.value = freqMap[color];
-		osc.start(state.ctx.currentTime);
-		state.gainNode.gain.exponentialRampToValueAtTime(1, state.ctx.currentTime + 0.01);
 		state.gainNode.connect(state.ctx.destination);
+		osc.start();
+		state.gainNode.gain.setValueAtTime(0.4, state.ctx.currentTime);
 		setTimeout(function() {
-			state.gainNode.gain.exponentialRampToValueAtTime(0.0001, state.ctx.currentTime + 0.01);
-			state.gainNode.disconnect();
-			osc.stop(state.ctx.currentTime);
-		}, K.ACTIVE_TIME);
+			state.gainNode.gain.setValueAtTime(state.gainNode.gain.value, state.ctx.currentTime);
+			state.gainNode.gain.exponentialRampToValueAtTime(0.0001, state.ctx.currentTime + RAMP_DOWN_TIME);
+			setTimeout(function() {
+				state.gainNode.disconnect();
+				osc.stop(state.ctx.currentTime);
+			}, RAMP_DOWN_TIME * 1000);
+		}, K.ACTIVE_TIME - RAMP_DOWN_TIME * 1000);
 	}
 }, audioType);
